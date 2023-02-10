a37bfae3ea9292bf66502977d034adf237e31aef
kc3977 <102318279+kc3977@users.noreply.github.com>
2023-02-10 16:47:14 -0500

    Update exercise4.cpp

--- dv/exercise4.cpp
+++ dv/exercise4.cpp
@@ -3,4 +3,29 @@
 
 TEST_CASE("Exercise 4") {
   VExercise4 model;
+  model.cs = 0;
+  model.alpha = 0b11111111;
+  model.beta = 0b11111111;
+  model.gamma = 0b11111111;
+  bool wrong = false;
+  for (model.sel = 0; model.sel < 4; model.sel ++){
+    model.eval();
+    if (model.out != 0) wrong = true;
+  }
+  model.cs = 1;
+  model.alpha = 0b11110000;
+  model.beta = 0b00001111;
+  model.gamma = 0b10101010;
+  for (model.sel = 0; model.sel < 4; model.sel ++){
+    model.eval();
+    if (model.sel == 0){ 
+      if(model.out != 0) wrong = true;}
+    if (model.sel = 1){
+      if(model.out != 0b11110000) wrong = true;}
+    if (model.sel = 2){
+      if(model.out != 0b00001111) wrong = true;}
+    if (model.sel = 3){
+      if(model.out != 0b11110000 & (0b00001111|0b10101010)) wrong = true;}
+  }
+  REQUIRE(wrong == false);
 }
