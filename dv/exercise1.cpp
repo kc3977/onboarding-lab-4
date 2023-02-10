a258e7445d4c27c29ca2049a5d8f9d0b4f160000
kc3977 <102318279+kc3977@users.noreply.github.com>
2023-02-10 16:44:29 -0500

    Update exercise1.cpp

--- dv/exercise1.cpp
+++ dv/exercise1.cpp
@@ -3,4 +3,22 @@
 
 TEST_CASE("Exercise 1") {
   VExercise1 model;
+  bool wrong = false;
+  for (model.op = 0; model.op < 4; model.op ++){
+    for (model.a = 0; model.a < 256; model.a ++){
+      for (model.b = 0; model.b < 256; model.b ++){
+        model.eval();
+        if(model.op == 0) {
+          if((model.a ^ model.b) != model.out) wrong = true;}
+        if(model.op == 1) {
+          if((model.a << model.b) != model.out) wrong = true;}
+        if(model.op == 2) {
+          if(model.b != 0){
+          if((model.a % model.b) != model.out) wrong = true;}}
+        if(model.op == 0) {
+          if((~(model.a & model.b)) != model.out) wrong = true;}
+      }
+    }
+  }
+  REQUIRE(wrong == false)
 }
